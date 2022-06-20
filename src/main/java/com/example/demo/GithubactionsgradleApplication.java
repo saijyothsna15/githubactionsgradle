ea33e3598367bee455d8b027a3d0a4636cb73fe3
sai <saijyotshna@gmail.com>
2022-06-20 14:39:45 +0530

    My First Commit.

---
+++ src/main/java/com/example/demo/GithubactionsgradleApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubactionsgradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubactionsgradleApplication.class, args);
+	}
+
+}
